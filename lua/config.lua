ed2198acdee70c28b0532697ae3013ca309b0300
Guruprakash Rajakkannu <guruprakash.js@gmail.com>
2023-02-16 19:41:20 +0100

    nomore vimrc

--- lua/config.lua
+++ lua/config.lua
@@ -1,54 +1,4 @@
-require("packer").startup({
-  function(use)
-    use("wbthomason/packer.nvim") -- Package manager
-    use("simeji/winresizer")
-    use({
-      "jackMort/ChatGPT.nvim",
-      config = function()
-        require("chatgpt").setup({
-          -- optional configuration
-        })
-      end,
-      requires = {
-        "MunifTanjim/nui.nvim",
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim"
-      }
-    })
-
-    -- PRESERVE ORDER
-    require("config.theme").setup(use)
-    require("config.bufferline").setup(use)
-    require("config.treesitter").setup(use)
-    require("config.project").setup(use)
-    require("config.telescope").setup(use)
-    require("config.neodev").setup(use)
-    require("config.lspconfig").setup(use)
-    require("config.mason").setup(use)
-    require("config.autocomplete").setup(use)
-    require("config.git").setup(use)
-    require("config.fold").setup(use)
-    require("config.nvim-tree").setup(use)
-    require("config.indent-blankline").setup(use)
-    require("config.autopair").setup(use)
-    require("config.dap").setup(use)
-    require("config.symbol-outline").setup(use)
-    require("config.trouble").setup(use)
-    require("config.cheatsheet").setup(use)
-    require("config.lualine").setup(use)
-    require("config.notes").setup(use)
-    require("config.mini").setup(use)
-    require("config.todo-comments").setup(use)
-    require("config.null-ls").setup(use)
-  end,
-  config = {
-    display = {
-      open_fn = require("packer.util").float,
-    },
-  },
-})
-
-vim.cmd[[
+vim.cmd [[
 syntax on
 
 set rnu
@@ -70,20 +20,8 @@ let mapleader=","
 set mouse=""
 set cursorline
 
-" --------------
-" --------------
-" PLUGINS CONFIG
-" --------------
-" --------------
-
-" Tabline
-" set showtabline=2
-
 " fugitive
 let g:github_enterprise_urls = ['https://github.bus.zalan.do']
-
-" Git gutter
-" nmap ghp <Plug>(GitGutterPreviewHunk)
 
 " My shortcuts
 nmap _ :NvimTreeToggle<cr>
@@ -109,6 +47,51 @@ comman  Dc :tabc
 " set timeoutlen=500
 ]]
 
+require("packer").startup({
+	function(use)
+		use("wbthomason/packer.nvim") -- Package manager
+		use("simeji/winresizer")
+		use("mg979/vim-visual-multi")
+		use("tpope/vim-commentary")
+		use("tpope/vim-fugitive")
+		use("tpope/vim-rhubarb")
+		use("mkitt/tabline.vim")
+		use("nvim-lua/plenary.nvim")
+		use("tpope/vim-surround")
+		use("nvim-treesitter/nvim-treesitter")
+		use("easymotion/vim-easymotion")
+
+		-- PRESERVE ORDER
+		require("config.theme").setup(use)
+		require("config.bufferline").setup(use)
+		require("config.treesitter").setup(use)
+		require("config.project").setup(use)
+		require("config.telescope").setup(use)
+		require("config.neodev").setup(use)
+		require("config.lspconfig").setup(use)
+		require("config.mason").setup(use)
+		require("config.autocomplete").setup(use)
+		require("config.git").setup(use)
+		require("config.fold").setup(use)
+		require("config.nvim-tree").setup(use)
+		require("config.indent-blankline").setup(use)
+		require("config.autopair").setup(use)
+		require("config.dap").setup(use)
+		require("config.symbol-outline").setup(use)
+		require("config.trouble").setup(use)
+		require("config.cheatsheet").setup(use)
+		require("config.lualine").setup(use)
+		require("config.notes").setup(use)
+		require("config.mini").setup(use)
+		require("config.todo-comments").setup(use)
+		require("config.null-ls").setup(use)
+	end,
+	config = {
+		display = {
+			open_fn = require("packer.util").float,
+		},
+	},
+})
 -- vim.opt.numberwidth = 3
 -- vim.opt.statuscolumn = "%=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum < 10 ? v:lnum . '  ' : v:lnum) : ''}%=%s"
 
